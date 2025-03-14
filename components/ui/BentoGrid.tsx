5d7e0f48a854a5eb4e29e56322acba8d2b65765b
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2025-03-14 21:09:55 +0100

    ..

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -3,16 +3,18 @@
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
 // Also install this npm i --save-dev @types/react-lottie
-import Lottie from "react-lottie";
+
+// import Lottie from "react-lottie";
 
 import { cn } from "@/lib/utils";
 import Image from "next/image";
-
 import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
+import dynamic from "next/dynamic";
 
+const Lottie = dynamic(() => import("react-lottie"), { ssr: false });
 export const BentoGrid = ({
   className,
   children,
